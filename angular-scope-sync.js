3227bda75f34347ebe2e651aec057cde19c9398d
Thomas Schulz <schulz@seamlessinteraction.com>
2014-11-12 11:55:20 +0100

    first version with prop sync and object sync

---
+++ angular-scope-sync.js
@@ -0,0 +1,69 @@
+/**
+ * Created by thomas.schulz on 12.11.2014.
+ */
+
+(function () { // start IIFE
+  'use strict';
+
+  angular.module('scope-sync', []).
+      provider('sync-service', function () {
+        // expose to provider
+        this.$get = function ($rootScope, $location, $log) {
+
+          var socket = io.connect();
+
+
+          function sync( scopeObj , scopensp ){
+
+
+            (function bindSync( defaultObj , objnsp ){
+              for(var defaultKey in defaultObj){
+                if ( defaultObj.hasOwnProperty(defaultKey) && !defaultKey.startsWith('$') && typeof defaultObj[defaultKey] !== 'function' ) {
+
+                  console.log(typeof defaultObj[defaultKey]);
+
+
+                  if( typeof defaultObj[defaultKey] === 'object' ){
+                    bindSync(defaultObj[defaultKey] , defaultKey + '.' );
+                  }else{
+
+                    (function ( obj , key  ) {
+                      var valueClosure = obj[key];
+
+                      var syncNamespace = (scopensp || "") + "/" + (objnsp || "") + key;
+
+                      // register for incoming changes from the server
+                      socket.on( syncNamespace , function(data){
+                        valueClosure = data;
+                        // apply the scope to notify changes
+                        scopeObj.$apply();
+                      });
+
+                      Object.defineProperty(obj, key , {
+                        get: function () {
+                          // return local stored data
+                          return valueClosure;
+                        },
+                        set: function(value){
+                          // send update to socket.io proxy
+                          socket.emit( syncNamespace , value);
+                          // update prob for getter
+                          valueClosure = value;
+                        }
+                      });
+                    })( defaultObj , defaultKey  );
+
+                  }// end else
+                }
+              }
+            })( scopeObj );
+
+
+          }
+          return { sync: sync };
+
+        }
+      });
+
+
+}()); // end IIFE
